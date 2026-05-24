c817dbb27b149bfac0098fd5e307232b227b14f2
shubhsingh12 <chhotetyagirtji@gmail.com>
2026-05-24 12:27:21 +0530

    Update CartService.java

--- src/main/java/com/satyam/service/CartService.java
+++ src/main/java/com/satyam/service/CartService.java
@@ -20,7 +20,7 @@ public class CartService {
 
     private final AdminController adminController;
 	
-	String URL="http://localhost:1998/cart/api/";
+	String URL="http://shopspherewebservice-production.up.railway.app/cart/api/";
 	RestTemplate restTemplate=new RestTemplate();
 
    
